4af201424b4c728cc8aff9dbe0ad18a8feaacff4
Mike Sportiello <mikesportiello@Mikes-MacBook-Pro.local>
2021-04-29 23:07:41 -0400

    updates to functions

--- RLSdatabaseupdater.R
+++ RLSdatabaseupdater.R
@@ -1,14 +1,23 @@
 #this script exists to update the RLS database
+#load libraries
 library(org.Mm.eg.db)
 library(org.Hs.eg.db)
-RLSdatabase<-read.csv('https://raw.githubusercontent.com/msport469/fluximplied/master/RLSdatabase.csv?token=ANC4YV2BBAO6ACQMKN6WLFLARNP7G',
+#load our already published database
+RLSdatabase<-read.csv('https://raw.githubusercontent.com/msport469/fluximplied/master/RLSdatabase.csv?token=ANC4YV7WZDYTMHNDGWEHUODARNWWK',
                       stringsAsFactors = F)
+#look at the top
 head(RLSdatabase)
-
+#if necessary, write the csv and manually edit/curate it to add new rate limiting steps.
+#map the IDs to create a column of mouse ENTREZIDs and human ENTREZIDs
 RLSdatabase$mouse.entrez <- mapIds(org.Mm.eg.db,keys = RLSdatabase$mouse.gene.symbol,
                                    keytype = 'SYMBOL',
                                    column = 'ENTREZID')
 RLSdatabase$human.entrez <- mapIds(org.Hs.eg.db,keys = RLSdatabase$human.gene.symbol,
                                    keytype = 'SYMBOL',
                                    column = 'ENTREZID')
+#look at the top
+head(RLSdatabase)
+#rewrite the csv so I can push it to github later and publish it.
 write.csv(RLSdatabase,'RLSdatabase.csv')
+
+sessionInfo()
--- fluximplied.R
+++ fluximplied.R
@@ -1,92 +1,48 @@
-library(org.Mm.eg.db)
-library(enrichR)
-#dbs <- listEnrichrDbs()
-genes<-c('Cpt1a','Idh2','Acadl')
-#db<-c('KEGG_2019_Mouse')
-res<-enrichr(genes,db)
-#kegg<-res[[1]]
-RLSgenes<-c('Pfk1','Fbp1','Idh2','Gys1','Pygl','Ppat','Cpsii','Cpsi','ACAC','Cpt1a')
-mapIds(org.Mm.eg.db,keys = RLSgenes,column = 'ENTREZID',keytype = 'SYMBOL')
-intersect(genes,RLSgenes)
-
-
-
-
-
-RLSgenes<-c('Pfkm','Pfkl','Fbp1','Idh2','Gys1','Pygl','Ppat','Cad','Cps1','Acaca','Cpt1a')
-RLSpathways<-c('glycolysis','glycolysis','gluconeogenesis','TCA cycle','glycogen synthesis','glycogenolysis','de novo purine synthesis',
-               'de novo pyrimidine synthesis','urea cycle','fatty acid synthesis','beta oxidation of fatty acids')
-RLS<-data.frame(RLSgenes,RLSpathways)
-subset<-subset(RLS,RLS$RLSgenes %in% genes)
-subset
-
-
-
-
-
-
-RLSgenes<-c('Pfkm','Pfkl','Fbp1','Idh2','Gys1','Pygl','Ppat','Cad','Cps1','Acaca','Cpt1a')
-RLSpathways<-c('glycolysis','glycolysis','gluconeogenesis','TCA cycle','glycogen synthesis','glycogenolysis','de novo purine synthesis',
-               'de novo pyrimidine synthesis','urea cycle','fatty acid synthesis','beta oxidation of fatty acids')
-RLS<-data.frame(RLSgenes,RLSpathways)
-subset<-subset(RLS,RLS$RLSgenes %in% genes)
-colnames(subset)<-c('Rate limiting step genes in your set','Metabolic pathway associated with it')
-
-
-
-intersect<-intersect(genes,RLSgenes)
-lengthintersect<-length(intersect)
-ifzero<-'There are no genes in your set that are in our rate limiting step database. Make sure you used mouse gene symbols like Cpt1a, not the human symbol CPT1A or the ENTREZ ID 1374'
-ifnotzero<-print(paste0('Your gene set has --------> ',lengthintersect,' <-------- genes that have been identified as encoding enzymes involved as rate-limiting steps in the gene set you provided'))
-ifelse(lengthintersect==0,ifzero,ifnotzero)
-subset
-
-
-
-
-
-RLSgenes<-c('Pfkm','Pfkl','Fbp1','Idh2','Gys1','Pygl','Ppat','Cad','Cps1','Acaca','Cpt1a')
-RLSpathways<-c('glycolysis','glycolysis','gluconeogenesis','TCA cycle','glycogen synthesis','glycogenolysis','de novo purine synthesis',
-               'de novo pyrimidine synthesis','urea cycle','fatty acid synthesis','beta oxidation of fatty acids')
-RLS<-data.frame(RLSgenes,RLSpathways)
-subset<-subset(RLS,RLS$RLSgenes %in% genes)
-colnames(subset)<-c('Rate limiting step genes in your set','Metabolic pathway associated with it')
-intersect<-intersect(genes,RLSgenes)
-lengthintersect<-length(intersect)
-ifzero<-'There are no genes in your set that are in our rate limiting step database. Make sure you used mouse gene symbols like Cpt1a, not the human symbol CPT1A or the ENTREZ ID 1374'
-ifnotzero<-print(paste0('Your gene set has --------> ',lengthintersect,' <-------- genes that have been identified as encoding enzymes involved as rate-limiting steps in the gene set you provided'))
-ifelse(lengthintersect==0,ifzero,ifnotzero)
-ifelse(lengthintersect==0,'Sorry about that.',subset)
-
-
-
-lengthintersect
-
-
-
-
-RLSdatabase<-read.csv('https://raw.githubusercontent.com/msport469/fluximplied/master/RLSdatabase.csv?token=ANC4YV6CMWQ3MBVHDWILXG3ARNO56')
-
-
-
-
-impliedflux <- function(genes) {
-  # function to see if there are any rate limiting steps in gene list
-  RLSgenes<-c('Pfkm','Pfkl','Fbp1','Idh2','Gys1','Pygl','Ppat','Cad','Cps1','Acaca','Cpt1a')
-  RLSpathways<-c('glycolysis','glycolysis','gluconeogenesis','TCA cycle','glycogen synthesis','glycogenolysis','de novo purine synthesis',
-                 'de novo pyrimidine synthesis','urea cycle','fatty acid synthesis','beta oxidation of fatty acids')
-  RLS<-data.frame(RLSgenes,RLSpathways)
-  subset<-subset(RLS,RLS$RLSgenes %in% genes)
-  colnames(subset)<-c('Rate limiting step genes in your set','Metabolic pathway associated with it')
-  intersect<-intersect(genes,RLSgenes)
-  lengthintersect<-length(intersect)
-  ifzero<-'There are no genes in your set that are in our rate limiting step database. Make sure you used mouse gene symbols like Cpt1a, not the human symbol CPT1A or the ENTREZ ID 1374'
-  ifnotzero<-paste0('Your gene set has --------> ',lengthintersect,' <-------- genes that have been identified as encoding enzymes involved as rate-limiting steps in the gene set you provided')
-  
-  print(ifelse(lengthintersect==0,ifzero,ifnotzero))
-  print(ifelse(lengthintersect==0,'Sorry about that.',subset))
+#create a function that makes sure the user formatted their data according to species and format correctly. 
+#If you tell the function you are giving us symbols when you are not, or otherwise lie to the function, it will give spurious results.
+specform <- function(species,format) {
+  ifelse(species=='Mmu', 
+         ifelse(format=='Symbol', RLSgenes<-RLSdatabase$mouse.gene.symbol, 
+                ifelse(format=='ENTREZID', RLSgenes<-RLSdatabase$mouse.entrez, print('Your species was accepted as Mmu, but your format was neither Symbol nor ENTREZID'))), 
+         ifelse(species=='Hsa',
+                ifelse(format=='Symbol', RLSgenes<-RLSdatabase$human.gene.symbol,
+                       ifelse(format=='ENTREZID', RLSgenes<-RLSdatabase$human.entrez,print('Your species was accepted as Hsa, but your format was neither Symbol nor ENTREZID'))),
+                print('Your species must be Mmu or Hsa')))
+  RLSgenes<<-RLSgenes
 }
 
+fluximplied <- function(genes,species,format) {
+  # function to see if there are any rate limiting steps in gene list
+  #load the rate limiting step database
+  #RLSdatabase<-read.csv('https://raw.githubusercontent.com/msport469/fluximplied/master/RLSdatabase.csv?token=ANC4YV6CMWQ3MBVHDWILXG3ARNO56',stringsAsFactors = F)
+  #convert the database that matches your data for species and format (Symbol or ENTREZID)
+  RLSgenes<-specform(species,format)
+  #save the pathways
+  RLSpathways<-RLSdatabase$metabolic.reaction
+  #make a dataframe with those genes and pathways
+  RLS<-data.frame(RLSgenes,RLSpathways)
+  #subset the database to onlyinclude genes in your set
+  subset<-subset(RLS,RLS$RLSgenes %in% genes)
+  #change the column names so the user knows what each column actually is
+  colnames(subset)<-c('RLS genes in your set','Pathway associated with gene')
+  #create an intersect so we can actually count them
+  intersect<-intersect(genes,RLSgenes)
+  lengthintersect<-length(intersect)
+  #create the responses if there are any genes left after subsetting on your genes
+  #that are also in our database for being rate limiting steps
+  ifzero<-paste('There are no genes in your set that are in our rate limiting step database. Make sure you gave the correct format of species (Mmu or Hsa only) and format (Symbol or ENTREZID only). Your genes should be in a character vector')
+  ifnotzero<-paste0('Your gene set has --------> ',lengthintersect,' <-------- genes that have been identified as encoding enzymes involved as rate-limiting steps in the gene set you provided.')
+  print(ifelse(lengthintersect==0,ifzero,ifnotzero))
+  print(ifelse(lengthintersect==0,'Sorry about that.','Your RLS genes are saved as myRLSgenes and a dataframe of genes and corresponding pathways is saved as myRLStable'))
+  #save the outputs so the user can hold onto them and look at them
+  myRLStable<<-subset
+  myRLSgenes<<-intersect(RLS$RLSgenes,genes)
+  #print the RLS database that has been subset to only include genes that are in user's list
+  subset
+}
 
-impliedflux(c('Idh2','Cpt1a'))
-
+#this is a test function
+genes<-c('Idh2','Cpt1a')
+species<-'Mmu'
+format<-'Symbol'
+fluximplied(genes=genes,species=species,format=format)
