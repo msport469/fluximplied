99ca1ef086e90cc5c00c520fb06a212062a9fb34
msport469 <sportiellomike@gmail.com>
2021-08-23 14:08:13 -0400

    Update prepareihwresult.R

--- prepareihwresult.R
+++ prepareihwresult.R
@@ -1,16 +1,13 @@
-library(ggplot2)
-library(viridis)
-library(enrichR)
-library(gridExtra)
-#load in deseqresult
-saveRDS(ihw.res.DPvsDN.all,'DPDNdf.RDS')
-inputdat<-readRDS('DPDNdf.RDS')
+#load in deseqresult from previous publication (Frontiers in Immunology 2021), then we'll save it
+#saveRDS(ihw.res.DPvsDN.all,'DPDNdfmouse.RDS')
+inputdat<-readRDS('DPDNdfmouse.RDS')
 inputdatgsea<-inputdat
 inputdat$gene<-rownames(inputdat)
+#subset on p values
 inputdatgsea <- inputdatgsea[inputdatgsea[,"weighted_pvalue"]<0.05,]
-inputdatgseaup <- inputdatgsea[inputdatgsea[,"log2FoldChange"]>0,]
-inputdatgseadown <- inputdatgsea[inputdatgsea[,"log2FoldChange"]<0,]
-
-
+#differentiate up and down
+inputdatgseaup <- inputdatgsea[inputdatgsea[,"log2FoldChange"]>0.5,]
+inputdatgseadown <- inputdatgsea[inputdatgsea[,"log2FoldChange"]<(-0.5),]
+#save genes as vectors.
 upgenes <- rownames(inputdatgseaup)
 downgenes <- rownames(inputdatgseadown)
