1d62614f2a9aa63ea0d0f48691a19e72a37b1b9b
msport469 <sportiellomike@gmail.com>
2021-08-24 09:07:14 -0400

    Update plottingcodeforadamtoputinhisscript.R

--- plottingcodeforadamtoputinhisscript.R
+++ plottingcodeforadamtoputinhisscript.R
@@ -4,14 +4,14 @@ library(viridis)
 library(enrichR)
 library(gridExtra)
 library(tidyr)
-
-#Enrichr
-#set up dbs
-dbs <- c("KEGG_2019_Human",'Reactome_2016')
+library(fluximplied)
 #read in humans
 AdiposevLiver<-readRDS('humandata/AdiposevLiver.RDS')
 AdiposevPutamen<-readRDS('humandata/AdiposevPutamen.RDS')
 LivervPutamen<-readRDS('humandata/LivervPutamen.RDS')
+write.csv(AdiposevLiver,'AdiposevLiver.csv')
+write.csv(AdiposevPutamen,'AdiposevPutamen.csv')
+write.csv(LivervPutamen,'LivervPutamen.csv')
 #p value subset
 inputdatgseaAvL <- AdiposevLiver[AdiposevLiver[,"padj"]<0.05,]
 inputdatgseaAvP <- AdiposevPutamen[AdiposevPutamen[,"padj"]<0.05,]
@@ -26,7 +26,7 @@ inputdatgseaAvPdown <- inputdatgseaAvP[inputdatgseaAvP[,"log2FoldChange"]<(-0.5)
 inputdatgseaLvPup <- inputdatgseaLvP[inputdatgseaLvP[,"log2FoldChange"]>0.5,]
 inputdatgseaLvPdown <- inputdatgseaLvP[inputdatgseaLvP[,"log2FoldChange"]<(-0.5),]
 
-####MOUSEYMOUSEYMOUSEY YEAH####
+####read in MOUSEYMOUSEYMOUSEY YEAH####
 inputdatgseamouse<-readRDS('mousedata/DPDNdfmouse.RDS')
 inputdatgseamouse$gene<-rownames(inputdatgseamouse)
 #subset on p values
@@ -40,13 +40,19 @@ downgenesmouse <- rownames(inputdatgseamousedown)
 upgenes<-upgenesmouse
 downgenes<-downgenesmouse
 
+#set up dbs for gsea
+dbs <- c("KEGG_2019_Human",'Reactome_2016')
+
 #get ready to plot 
 list_up <- c(upgenes)
 list_down <- c(downgenes)
 eup <- enrichr(list_up, dbs)
 edown <- enrichr(list_down, dbs)
 
-#KEGG Mouse
+##### Mouse #####
+##### Mouse #####
+##### Mouse #####
+
 #####there are no up or down pathways in KEGG mouse cutoff lfc=0.5 or -0.5
 # up <- eup$KEGG_2019_Mouse
 # down <- edown$KEGG_2019_Mouse
@@ -92,7 +98,7 @@ gos <- rbind(down,up)
 gos <- na.omit(gos) # Diverging Barcharts
 gos$logpadj <- -log10(gos$Adjusted.P.value)
 gos$Overlap[1:10] <- -(gos$Overlap[1:10])
-ggplot(gos, aes(x=reorder(Term,Overlap), y=Overlap , label=Overlap)) + 
+reactmouse<-ggplot(gos, aes(x=reorder(Term,Overlap), y=Overlap , label=Overlap)) + 
   geom_bar(stat='identity', aes(fill=logpadj), width=.5,position="dodge")  +
   scale_fill_viridis() + 
   # labs(title= "") +
@@ -101,27 +107,15 @@ ggplot(gos, aes(x=reorder(Term,Overlap), y=Overlap , label=Overlap)) +
   xlab(NULL)+
   labs(fill = bquote('-log P'['adj']))+
   coord_flip()
-reactomebarplot
 
+reactmouse
 
-
-
-
-
-
-
-#we need to add lines of code to read in files. 
-#adam, can you please put .RDS files in subfolder of fluximplied dev called
-#human data
-readRDS('humandata/AdiposevLiver.RDS')
-
-list_up <- c(upgenes)
-list_down <- c(downgenes)
-
-eup <- enrichr(list_up, dbs)
-edown <- enrichr(list_down, dbs)
-
-#KEGG Hoooman
+write.csv(eup$Reactome_2016,'enrichrcsvs/upreactome-mouse-LFC_0.5_.csv')
+write.csv(edown$Reactome_2016,'enrichrcsvs/downreactome-mouse-LFC_0.5_.csv')
+###### Hoooman #####
+###############
+#############
+###### Hoooman #####
 up <- eup$KEGG_2019_Human
 down <- edown$KEGG_2019_Human
 up$type <- "up"
@@ -195,22 +189,27 @@ ggplot(gos, aes(x=reorder(Term,Overlap), y=Overlap , label=Overlap)) +
   coord_flip()
 reactomebarplot
 
-
-
-
-
-
-
-
-
-
-
-write.csv(eup$KEGG_2019_Human,'./compare/upKEGG_2019_Human-LFC_0.5_.csv')
-write.csv(eup$Reactome_2016,'./compare/upReactome_2016-LFC_0.5_.csv')
+##### write human csvs #####
+write.csv(eup$KEGG_2019_Human,'./comparecsvs/upKEGG_2019_Human-LFC_0.5_.csv')
+write.csv(eup$Reactome_2016,'./comparecsvs/upReactome_2016-LFC_0.5_.csv')
 
 write.csv(edown$KEGG_2019_Human,'./compare/downKEGG_2019_Human-LFC_0.5_.csv')
 write.csv(edown$Reactome_2016,'./compare/downReactome_2016-LFC_0.5_.csv')
 
+
+
+##### fluximplied stuff #####
+fluximplied(AdiposevLiver,padjcolname = 'padj')
+fluximplied(AdiposevPutamen,padjcolname = 'padj')
+fluximplied(LivervPutamen,padjcolname = 'padj')
+
+
+#### gridarranging plots ####
+
 barplots<-grid.arrange(arrangeGrob(reactomebarplot,keggbarplot,nrow=1,widths=c(1.5,1)))
 barplots
 ggsave('humanbarplots.png',plot=barplots,dpi=600,path='./compare/',units = 'cm',width=16.6,height=6,scale=2)
+
+
+
+
